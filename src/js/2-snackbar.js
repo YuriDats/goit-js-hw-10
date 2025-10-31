7257b320f32f6eca2a4d9be963725287ec3e6f37
YuriDats <datsiuk.ukr@gmail.com>
2025-10-31 23:43:44 +0200

    push

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(event.target.elements.delay.value);
+  const state = event.target.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(ms => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(ms => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    });
+});
